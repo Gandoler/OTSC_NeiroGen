bb82817c84140b62f7fd898cd6c81398cdba2bbf
Gandoler <gl.krutoi@mail.ru>
2025-03-14 19:46:39 +0300

    добавил логирование запросов и свагер

--- NeiroGenApi/Program.cs
+++ NeiroGenApi/Program.cs
@@ -1,10 +1,22 @@
 
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddOpenApi();
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console() 
+    .WriteTo.File("logs/log.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+builder.Host.UseSerilog();
+
 
 var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseSerilogRequestLogging();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
