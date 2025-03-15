c0be24fc78d91bfeb2f1756ae36330dc808931fa
Gandoler <gl.krutoi@mail.ru>
2025-03-15 12:58:34 +0300

    первый запуск без теста

--- NeiroGenApi/Program.cs
+++ NeiroGenApi/Program.cs
@@ -1,4 +1,3 @@
-
 using Domain.Services;
 using Domain.Services.IServices;
 using Infrastructure.Business.SERVICES;
@@ -7,45 +6,40 @@ using Serilog;
 var builder = WebApplication.CreateBuilder(args);
 
 var configuration = builder.Configuration;
-var apiKey = configuration["ApiSettings:ApiKey"];
-var apiUrl = configuration["ApiSettings:ApiUrl"];
+var apiKey = configuration["ApiSettings:ApiKey"] ?? throw new Exception("ApiKey is missing");
+var apiUrl = configuration["ApiSettings:ApiUrl"] ?? throw new Exception("ApiUrl is missing");
 
-// Регистрация зависимостей
-builder.Services.AddScoped<IGenerateCongratilation>(provider =>
+builder.Services.AddHttpClient();
+
+// Регистрация сервисов напрямую через DI
+builder.Services.AddScoped<IGenerateCongratilation, GenerateCongratulations>(provider =>
 {
     var httpClient = provider.GetRequiredService<HttpClient>();
     return new GenerateCongratulations(apiKey, apiUrl, httpClient);
 });
 
-builder.Services.AddScoped<IGenerateCongratilation, GenerateCongratulations>();
 builder.Services.AddScoped<IProxyApiClient, ProxyApiClient>();
-builder.Services.AddHttpClient();
 builder.Services.AddScoped<IAddNewCongratulationsService, AddNewCongratulationsService>();
-builder.Services.AddControllers();
 
+builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+
 Log.Logger = new LoggerConfiguration()
-    .WriteTo.Console() 
+    .WriteTo.Console()
     .WriteTo.File("logs/log.txt", rollingInterval: RollingInterval.Day)
     .CreateLogger();
 
 builder.Host.UseSerilog();
-
 
 var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 app.UseSerilogRequestLogging();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
-
-
-
 app.Run();
-
